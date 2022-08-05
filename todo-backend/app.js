8589d923bf920fb4a31b16b9070bed891f1cb695
glooneltharion <gloo.neltharion@gmail.com>
2022-08-05 11:57:55 +0200

    add sample.env to backend

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const app = express();
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const env = require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
